5f1994e6ac931dfe42a5e6d9d5b406f006387943
grisselVelazquez03 <L20650227@zitacuaro.tecnm.mx>
2025-03-17 23:11:55 -0600

    agrega funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 console.log("Ejecutando el sistema")
 
 let listaAmigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value.trim();
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+    if (listaAmigos.includes(nombre)) {
+        alert("Este nombre ya ha sido agregado.");
+        return;
+    }
+    listaAmigos.push(nombre);
+    actualizarListaAmigos();
+    document.getElementById('amigo').value = "";
+}
